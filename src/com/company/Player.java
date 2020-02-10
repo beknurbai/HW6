f2bae64dad034010c73f0d36628311ddbd132bba
beknurbaihan <beknurbaihan@gmail.com>
2020-02-10 20:59:42 +0600

    Initial commit

---
+++ src/com/company/Player.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Player {private int health;
+private int damage;
+private String specialAbbility;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getSpecialAbbility() {
+        return specialAbbility;
+    }
+
+    public void setSpecialAbbility(String specialAbbility) {
+        this.specialAbbility = specialAbbility;
+    }
+}
---
+++ src/com/company/Warrior.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Warrior extends Player {private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
